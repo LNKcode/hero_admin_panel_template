1a0d737c19421518549ba68a7da5447c8346d10e
LNKcode <3-tv-n@mail.ru>
2025-10-24 23:01:31 +0300

    add createAsyncThunk ps dont remove void file

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
-import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware} from 'redux'
-import { thunk } from 'redux-thunk'
-import heroes from '../reducers/heroes';
-import filters from '../reducers/filters';
+import { configureStore } from '@reduxjs/toolkit';
+import heroes from '../components/heroesList/heroesSlice.js'
+import filters from '../components/heroesFilters/filtersSlice.js';
 
 
 const stringMiddleware = (store) => (next) => (action) => {
@@ -14,11 +13,11 @@ const stringMiddleware = (store) => (next) => (action) => {
 }
 
 
-const store = createStore(
-                    combineReducers({heroes, filters}), 
-                    compose(applyMiddleware(thunk, stringMiddleware), 
-                    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
-                  );
+const store = configureStore({
+  reducer: {heroes, filters},
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+  devTools: process.env.NODE_ENV !== 'production',
+  
+})
 
 export default store;
